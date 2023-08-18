552d9610b3f6e4f6807abe735ac0bd891a67b9ab
vgont <vgontijo34@gmail.com>
2023-08-18 10:37:22 -0300

    first page next

---
+++ src/app/sobre/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Sobre() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>essa é a página sobre</p>
+      <Link href="/">Home</Link>
+    </main>
+  )
+}
